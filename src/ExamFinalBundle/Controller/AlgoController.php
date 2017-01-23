08406e5b1a9476ba4abd35894ea2efd6402e4d50
gthielon <gthielon@gmail.com>
2017-01-23 10:56:06 +0100

    Exam algo wcs

--- src/ExamFinalBundle/Controller/AlgoController.php
+++ src/ExamFinalBundle/Controller/AlgoController.php
@@ -13,12 +13,29 @@ class AlgoController extends Controller
     // Exercice 1
     public function traduire($string)
     {
+        $voyelles = ['a', 'e', 'i', 'o', 'u'];
+        $array = str_split($string);
+        $array_new = array();
 
+        foreach ($array as $value) {
+            array_push($array_new, $value);
+            if (!in_array($value, $voyelles)) {
+                array_push($array_new, 'av');
+            }
+        }
     }
+
+
 
     // Exercice 2
     public function action($input)
     {
-
+        $search = 1;
+        $replace = 2;
+        for($i=0;$i<count($input);$i++) {
+            if($input[$i] == $search) {
+                $input[$i] = $replace;
+            }
+        }
     }
 }
