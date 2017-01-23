6a198faa05c7dceac31792b4951a05563dc6a495
Jpilosel <formation@pilosel.fr>
2017-01-23 11:00:33 +0100

    exam algo

--- src/ExamFinalBundle/Controller/AlgoController.php
+++ src/ExamFinalBundle/Controller/AlgoController.php
@@ -13,12 +13,53 @@ class AlgoController extends Controller
     // Exercice 1
     public function traduire($string)
     {
+//        $stringLower = mb_strtolower($string);
+        $tabs = explode(' ', $string);
+        $result = null;
+        foreach ($tabs as $tab) {
+
+            $caractere= str_split($tab);
+            $nbCaractere=count($caractere);
+                for ($i=1; $i< $nbCaractere; $i++){
+                    if ($caractere[$i] == $caractere[$i-1]){
+                        $result .= $caractere[$i];
+                    }elseif($caractere[$i]='a' || $caractere[$i]='e' ||$caractere[$i]='i' ||$caractere[$i]='o' ||$caractere[$i]='u' ||$caractere[$i]='y'){
+                        $result .= 'av'.$caractere[$i];
+
+                    }else{
+                        $result .= $caractere[$i];
+                    }
+                }
+            $result.=' ';
+        }
+    return $result;
+
 
     }
 
     // Exercice 2
     public function action($input)
     {
+        $nbligne=0;
+        foreach ($input as $value) {
+            $nbligne++;
+        }
+        foreach ($input as $key=>$ligne) {
+            $colone=count($ligne);
+            if($key !=0 || $key!=$nbligne){
+                for ($i=1; $i<$nbligne; $i++) {
+                    foreach ($ligne as $keycase => $case) {
+                        if ($keycase !=0 ||$keycase != $colone) {
+                            if($case[$keycase-1]>=1 && $ligne[$key-1][$keycase]>=1){
+                                $input[$key][$keycase]= 2;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        return $input;
 
     }
 }
