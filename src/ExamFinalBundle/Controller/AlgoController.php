2e9fcc4572403a2f4dc862af592f2152abc5a518
Anne-Laure Fontaine <anne.lfontaine@gmail.com>
2017-01-23 10:41:29 +0100

    sauvegarde

--- src/ExamFinalBundle/Controller/AlgoController.php
+++ src/ExamFinalBundle/Controller/AlgoController.php
@@ -13,12 +13,23 @@ class AlgoController extends Controller
     // Exercice 1
     public function traduire($string)
     {
-
+        $string = str_split(strtolower($string));
+        $pattern = ['a', 'e', 'é', 'è', 'i', 'o', 'u', 'ù', 'y'];
+        $tab = [];
+        foreach ($string as $letters) {
+            if (in_array($pattern, $tab)) {
+                $tab[$letters].+'av';
+            }
+            return implode("",$tab);
+        }
     }
 
     // Exercice 2
     public function action($input)
     {
-
+        for($i = 1; $i<=count($input); $i++) {
+                $input[$i][1] = $input[$i][1] + 1;
+            }
+            return $input;
     }
 }
