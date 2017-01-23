1361a0ad4216dff4c8522617c162830460c19f21
leclercweb <lavega45@yahoo.fr>
2017-01-23 10:47:44 +0100

    envoi premier algo

--- src/ExamFinalBundle/Controller/AlgoController.php
+++ src/ExamFinalBundle/Controller/AlgoController.php
@@ -14,11 +14,31 @@ class AlgoController extends Controller
     public function traduire($string)
     {
 
+        $string = strtolower($string);
+        $phrase = explode(' ', $string);
+        $table = str_split($phrase);
+
+        $voyelle = '/[aeiouy]/';
+        $voyellefin = "#[aeiouy]$#";
+
+        $javanais = "av";
+
+        if(preg_match($voyelle, $phrase )) {
+            echo $javanais, $string;
+        }
+        elseif(preg_match($voyellefin, $phrase) && (preg_match($voyellefin, $phrase) == "e")){
+            echo $string;
+        }
+
+
+
     }
 
     // Exercice 2
     public function action($input)
     {
-
+        $caract = "1";
+        $result = str_replace($caract, "2", $string);
+      
     }
 }
