e15ed8b0efca6c06ea3dedf86a7d8930f7f62385
Jasdero <houssemaine.j@gmail.com>
2017-01-23 10:56:58 +0100

    mayday

--- src/ExamFinalBundle/Controller/AlgoController.php
+++ src/ExamFinalBundle/Controller/AlgoController.php
@@ -15,27 +15,50 @@ class AlgoController extends Controller
     {
         $string = strtolower($string);
         $words = explode(' ', $string);
-        $output= $javanese = [];
+        $output = $javanese = [];
 
         $pattern = '/[aeiouy]/';
         foreach ($words as $word) {
             $letters = str_split($word);
-            for ($i =0; $i<count($letters); $i++) {
+            for ($i = 0; $i < count($letters); $i++) {
 
                 if (preg_match($pattern, $letters[$i])) {
-                    $output[] = 'av'.$letters[$i];
+                    $output[] = 'av' . $letters[$i];
                 } else {
                     $output[] = $letters[$i];
                 }
 
-            }$javanese[] = implode($output);
+            }
+            $javanese[] = implode($output);
         }
-        return implode(' ',$javanese);
+        return implode(' ', $javanese);
     }
 
     // Exercice 2
     public function action($input)
     {
 
+
+        $output = [];
+        foreach ($input as $value => $key) {
+
+                if ($value[$key] <= 2) {
+                    $output[$key][] = 2;
+
+                } else {
+                    $output[$key][] = 1;
+                }
+
+        }
+
+
+        /*for ($i = 1 ; $i<count($input);$i++) {
+            if ($input[$i]<=2){
+                $output[$i] = 2;
+            } else{
+                $output[$i] = $input[$i];
+            }*/
+        return $output;
     }
+
 }
