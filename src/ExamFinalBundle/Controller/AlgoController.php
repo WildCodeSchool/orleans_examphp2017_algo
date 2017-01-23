61050f49461b75838b0a3204627692ed36cbfe8d
Kim Nguyen <knguyen.wcs@gmail.com>
2017-01-23 10:42:11 +0100

    exam terminé

--- src/ExamFinalBundle/Controller/AlgoController.php
+++ src/ExamFinalBundle/Controller/AlgoController.php
@@ -13,12 +13,29 @@ class AlgoController extends Controller
     // Exercice 1
     public function traduire($string)
     {
-
+        $string = strtolower($string);
+        $caracteres=preg_split('//',$string,-1,PREG_SPLIT_NO_EMPTY);
+        print_r($caracteres);
     }
 
     // Exercice 2
     public function action($input)
     {
-
+        //Boucle de construction et de remise à zéro
+        for ($i = 0; $i < 6;$i++)
+        {
+            for ($g = 0; $g < 7;$g++)
+            {
+                $tableau[$i][$g] = "Vide";
+            }
+        }
+//Boucle d'affichage
+        for ($i = 0; $i < 6;$i++)
+        {
+            for ($g = 0; $g < 7;$g++)
+            {
+                echo $tableau[$i][$g];
+            }
+        }
     }
 }
