016147b8c949fa929bfe023792426ca230c6e193
MathieuM1 <mat.mercier45@gmail.com>
2017-01-23 11:05:10 +0100

    youpi...

--- src/ExamFinalBundle/Controller/AlgoController.php
+++ src/ExamFinalBundle/Controller/AlgoController.php
@@ -13,8 +13,28 @@ class AlgoController extends Controller
     // Exercice 1
     public function traduire($string)
     {
+//        $voyelles = array("a", "e", "i", "o", "u", "y");
+//        $string = strtolower($string);
+//        $string = str_replace($voyelles, "av", $string);
+//
+//        return $string;
 
+        $string = strtolower($string);
+        $letters = str_split($string);
+        $pattern = '/[aeiouy]/';
+
+        foreach ($letters as $letter) {
+            if (preg_match($pattern, $letter)) {
+                $javanais = 'av'.$letter;
+                return $javanais;
+
+            } else {
+                return implode($letters);
+            }
+        }
     }
+
+
 
     // Exercice 2
     public function action($input)
