65e112e2a230c9e5ab29601d98359805751aaa83
Messant <messant.d@gmail.com>
2017-01-23 11:05:02 +0100

    :(

--- src/ExamFinalBundle/Controller/AlgoController.php
+++ src/ExamFinalBundle/Controller/AlgoController.php
@@ -13,12 +13,33 @@ class AlgoController extends Controller
     // Exercice 1
     public function traduire($string)
     {
+        $string ='';
+        $add='av';
+        foreach ($string as $add)
+        if (preg_match('#[aeiouy]'.$add, $string)) {
+
+            $result =$add.$string;
+        }elseif{
+
+        }else{
+
+        }
+        return strtolower($result);
+
 
     }
 
     // Exercice 2
     public function action($input)
     {
+      $tab1='0';
+      $tab2='1';
+        foreach ($tab1 as $tab2){
+            if ($tab1==0){
+
+            }
+        }elseif ($tab2==1){
 
     }
+
 }
