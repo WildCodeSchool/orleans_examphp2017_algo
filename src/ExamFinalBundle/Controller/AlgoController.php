4e29fb9ed13edec9ea715f64a48c4c478cdeffe3
ludovicmoreau <ludovic_moreau@laposte.net>
2017-01-23 10:45:22 +0100

    commit initial

--- src/ExamFinalBundle/Controller/AlgoController.php
+++ src/ExamFinalBundle/Controller/AlgoController.php
@@ -13,6 +13,22 @@ class AlgoController extends Controller
     // Exercice 1
     public function traduire($string)
     {
+        $string = strtolower($string);
+
+        $caractères  = str_split($string);
+        $javanais = ['av'];
+        $resultat =[];
+        foreach( $caractères as $voyelles )
+        {
+            if (preg_match('/[aeiouy]{1}/', $voyelles))
+            {
+                $resultat = array_merge($javanais, $caractères);
+            }
+
+
+        }
+
+    return implode ("", $resultat );
 
     }
 
@@ -20,5 +36,44 @@ class AlgoController extends Controller
     public function action($input)
     {
 
+        $array1 =[];
+        $array2 =[];
+        $array3 =[];
+        $array4 =[];
+        $array5 =[];
+        $array6 =[];
+        $array7 =[];
+        $array8 =[];
+        $array9 =[];
+
+        $result1 =[];
+        $result2 =[];
+        $result3 =[];
+        $result4 =[];
+        $result5 =[];
+        $result6 =[];
+        $result7 =[];
+        $result8 =[];
+        $n = 0;
+        foreach ( $input as $key => $value)
+        {
+            if ($value[$key[$n]] =1 and $value[$key[$n+1]] =1 )
+            {
+                $result1 = $array2[$key[$n+1]] = 2;
+                $result2 = $array3[$key[$n+1]] = 2;
+                $result3 = $array4[$key[$n+1]] = 2;
+                $result4 = $array5[$key[$n+1]] = 2;
+                $result5 = $array6[$key[$n+1]] = 2;
+                $result6 = $array7[$key[$n+1]] = 2;
+                $result7 = $array8[$key[$n+1]] = 2;
+                $result8 = $array9[$key[$n+1]] = 2;
+
+            }
+            $n+=1;
+        }
+         return $resultfinal = array_merge (  $array1 ,[$result1],[$result2],[$result3],[$result4],[$result5],[$result6],[$result7],[$result8 ]);
+
+
     }
+
 }
